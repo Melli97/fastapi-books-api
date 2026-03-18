aa6d4319fcf721889496b49450f3e118fb31dddf
Marcello Melli <marcellomelli9@gmail.com>
2026-03-18 15:47:02 +0100

    primo commit fastapi

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+libri = {
+    "libro1": {
+        "titolo": "Il nome della rosa",
+        "autore": "Umberto Eco",
+        "categoria": "Romanzo storico"
+    },
+    "libro2": {
+        "titolo": "1984",
+        "autore": "George Orwell",
+        "categoria": "Distopico"
+    },
+    "libro3": {
+        "titolo": "Il Signore degli Anelli",
+        "autore": "J.R.R. Tolkien",
+        "categoria": "Fantasy"
+    }
+}
+
+@app.get("/")
+def home():
+    return {"messaggio": "Funziona!"}
+
+@app.get("/libri")
+async def read_all_books():
+    return libri
