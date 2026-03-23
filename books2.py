efcb9b21357094f804000aad8b9440120a312e89
Marcello Melli <marcellomelli9@gmail.com>
2026-03-23 16:46:48 +0100

    Data Validation Fields

--- books2.py
+++ books2.py
@@ -1,5 +1,7 @@
-from fastapi import Body, FastAPI
-from pydantic import BaseModel
+from typing import Optional
+
+from fastapi import  FastAPI
+from pydantic import BaseModel, Field
 app = FastAPI()
 
 class Book:
@@ -18,11 +20,12 @@ class Book:
         self.rating = rating
 
 class BookRequest(BaseModel):
-    id: int
-    title: str
-    author: str
-    description: str
-    rating : int
+   
+    id: Optional[int] = None  #id opzionale non obbligatori  non passiamo nulla lo passeremo dopo con la funzione
+    title: str = Field(min_lenght=3)
+    author: str =  Field(min_lenght=3)
+    description: str = Field(min_lenght=3, max_length=100)
+    rating : int = Field(gt=1,lt =6)
 
 
 
@@ -45,6 +48,23 @@ async def create_book(book_request: BookRequest):  # Riceve i dati del libro val
     new_book = Book(**book_request.model_dump())  # ** lasterisco davanti ci permette di assegnare questi argomenti al nuovo libro
     # Converte il modello Pydantic in dizionario e lo usa per creare un oggetto Book
     
-    BOOKS.append(new_book)  
-    # Aggiunge il nuovo libro alla lista in memoria (simula un database)           
+    BOOKS.append(find_book_id(new_book))  
+    # Aggiunge il nuovo libro alla lista in memoria
+     
+              
+def find_book_id(book: Book):  
+    # Funzione che assegna un ID univoco a un libro
 
+    if len(BOOKS) > 0:  
+        # Controlla se la lista BOOKS contiene già elementi
+
+        book.id = BOOKS[-1].id + 1  
+        # Prende l'ultimo libro della lista (BOOKS[-1])
+        # Legge il suo id e aggiunge 1 → così crea un nuovo id progressivo
+
+    else:
+        book.id = 1  
+        # Se la lista è vuota, assegna il primo id = 1
+
+    return book  
+    # Restituisce il libro con l'id aggiornato
