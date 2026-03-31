f348abfe833f509b7b7e7703542f759f37f9160b
Marcello Melli <marcellomelli9@gmail.com>
2026-03-31 16:49:13 +0200

    todo by id

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,10 +1,10 @@
 from typing import Annotated  # Importa Annotated per annotare il tipo di dipendenze
 from sqlalchemy.orm import Session  # Importa la classe Session da SQLAlchemy per gestire le sessioni del database
-from fastapi import FastAPI, Depends  # Importa FastAPI per creare l'app e Depends per gestire le dipendenze
+from fastapi import FastAPI, Depends, HTTPException, Path  # Importa FastAPI per creare l'app e Depends per gestire le dipendenze
 import models  # Importa il modulo models, che contiene i modelli del database
 from models import Todos  # Importa il modello Todos dal modulo models
 from database import engine, SessionLocal  # Importa l'engine del database e la classe di sessione locale
-
+from starlette import status
 # Crea un'istanza dell'app FastAPI
 app = FastAPI()
 
@@ -23,9 +23,28 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 # Endpoint per leggere tutte le voci dalla tabella Todos
-@app.get("/")
+@app.get("/", status_code= status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()  # Esegue una query per ottenere tutte le voci nella tabella Todos
 
 
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)  # Se la richiesta ha successo, restituisce HTTP 200
+
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    # db: viene iniettato automaticamente tramite la dependency (sessione DB)
+    # todo_id: parametro preso dall'URL
+    # Path(gt=0): valida che todo_id sia > 0
+
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    # Query al database:
+    # - cerca nella tabella Todos
+    # - filtra per id uguale a todo_id
+    # - prende il primo risultato trovato (oppure None se non esiste)
+
+    if todo_model is not None:
+        return todo_model     # Se il todo esiste, lo restituisce come risposta
+   
+    raise HTTPException(status_code=404, detail='todo non trovato')
+    # Se non esiste nessun record con quell'id:
+    # - restituisce errore 404 (Not Found)
 
